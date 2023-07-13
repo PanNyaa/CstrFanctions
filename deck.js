95901c580780b0d8d98f52e3749a0c8c38275d27
PanNyaa <pan.nyaa.ccc@gmail.com>
2023-07-13 22:15:50 +0900

    Update deck.js

--- deck.js
+++ deck.js
@@ -1,4 +1,12 @@
-/* test */
-window.onload = function() {
-document.body.insertAdjacentHTML('beforeend', '<iframe src="https://pannyaa.github.io/rss/" width="100%" height="600" frameborder="0" target="_blank" rel="noreferrer noopener"></iframe>');
-};
+(function() {
+  // iframeを作成します
+  var iframe = document.createElement('iframe');
+
+  // iframeの属性を設定します
+  iframe.src = 'http://twitter.com';
+  iframe.width = '500px';
+  iframe.height = '300px';
+
+  // iframeをbodyの末尾に追加します
+  document.body.appendChild(iframe);
+})();
