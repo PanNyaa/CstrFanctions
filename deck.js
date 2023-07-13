2cca7ed5a9424b7863286ee9f7abd7952e1a2210
PanNyaa <pan.nyaa.ccc@gmail.com>
2023-07-13 22:08:07 +0900

    Update deck.js

--- deck.js
+++ deck.js
@@ -1,4 +1,4 @@
 /* test */
 window.onload = function() {
-document.getElementById('note.com.frame').insertAdjacentHTML('beforeend', '<iframe src="https://pannyaa.github.io/rss/" width="100%" height="600" frameborder="0" target="_blank" rel="noreferrer noopener"></iframe>');
+document.body.insertAdjacentHTML('beforeend', '<iframe src="https://pannyaa.github.io/rss/" width="100%" height="600" frameborder="0" target="_blank" rel="noreferrer noopener"></iframe>');
 };
